7a15b90cd9263ee24a1be327982cbe17cea84d8f
vassdoki <laszlo.vass@imind.eu>
2017-04-27 22:20:55 +0200

    Experiment to show how the saved images are processed.

--- src/darts/experimental/CheckCalibrationImages.scala
+++ src/darts/experimental/CheckCalibrationImages.scala
@@ -11,26 +11,34 @@ import org.bytedeco.javacpp.opencv_imgproc.putText
   * Created by vassdoki on 2017.04.27..
   */
 object CheckCalibrationImages extends App{
-  val camConf = new Config("1")
-  val d = new File("/home/vassdoki/darts/v3/d-calibration/jo-kalibracio")
-  val listf = d.listFiles.filter(_.isFile)
-  val filter = s"d${camConf.int("videoDeviceNumber")}"
-  val filtered = listf.filter(_.getName.startsWith(filter))
+  check("1")
+  check("2")
+
+  def check(camId: String) {
+    val camConf = new Config(camId)
+    val d = new File("/home/vassdoki/darts/v3/d-calibration/jo-kalibracio")
+    val listf = d.listFiles.filter(_.isFile)
+    val filter = s"d${camConf.int("videoDeviceNumber")}"
+    val filtered = listf.filter(_.getName.startsWith(filter))
 
 
-  val capture1 = new CaptureDevice("1")
-  val camConf1 = new Config("1")
-  val dartFinder1 = new DartFinder(capture1, camConf1)
-  dartFinder1.state = dartFinder1.State.ALWAYS_ON
+    val capture = new CaptureDevice(camId)
+    val dartFinder = new DartFinder(capture, camConf)
+    dartFinder.state = dartFinder.State.ALWAYS_ON
 
-  for(file <- filtered.sorted) {
-    var i = imread(s"${file.getPath}")
-    dartFinder1.proc(i)
-    val d = dartFinder1.debugLastProc
-    Util.show(d, s"test")
-    imwrite(s"/home/vassdoki/darts/v3/t/${file.getName}", d)
-//    Thread.sleep(10)
-    CvUtil.releaseMat(d)
+    for (file <- filtered.sorted) {
+      var i = imread(s"${file.getPath}")
+      val x = dartFinder.proc(i)
+
+      val name = file.getName.substring(3).replace(".jpg", "")
+      println(f"CalibPoint$camId;$name;${x/camConf.int("area/width")}%15f;$x;${camConf.int("area/zeroy")}")
+
+      val d = dartFinder.debugLastProc
+      Util.show(d, s"test")
+      imwrite(s"/home/vassdoki/darts/v3/t/${file.getName}", d)
+      //    Thread.sleep(10)
+      CvUtil.releaseMat(d)
+    }
   }
 
 }
